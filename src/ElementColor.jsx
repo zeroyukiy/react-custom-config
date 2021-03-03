1a71d6238fc4d7bb8420b4b8a4d58db6a0fff176
zeroyuki <zeroyukiy@hotmail.com>
2021-03-03 18:20:58 +0100

    first commit

---
+++ src/ElementColor.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./style.css";
+
+export default class ElementColor extends Component {
+  render() {
+    return (
+      <>
+        <h2>Element Color</h2>
+        <ul className="list">
+          <li>hi</li>
+          <li>hello</li>
+          <li>world</li>
+          <li>bb</li>
+          <li>vvvv</li>
+        </ul>
+      </>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "src/assets/test.html",
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.m?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  // watch: true,
+};
